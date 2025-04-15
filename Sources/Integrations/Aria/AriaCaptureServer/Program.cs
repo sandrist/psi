33cc0d99489c695de5874f9756eef3653dd34466
sandrist <sandrist@microsoft.com>
2025-04-14 18:04:51 -0700

    adding audio and imu to Psi capture server

--- Sources/Integrations/Aria/AriaCaptureServer/Program.cs
+++ Sources/Integrations/Aria/AriaCaptureServer/Program.cs
@@ -6,6 +6,7 @@ namespace AriaCaptureServer
     using System;
     using System.Linq;
     using Microsoft.Psi;
+    using Microsoft.Psi.Audio;
     using Microsoft.Psi.Imaging;
     using Microsoft.Psi.Interop.Format;
     using Microsoft.Psi.Interop.Transport;
@@ -16,6 +17,12 @@ namespace AriaCaptureServer
         {
             using var pipeline = Pipeline.Create();
             var store = PsiStore.Create(pipeline, "AriaStreams", @"C:\Temp");
+
+            var audioSource = new NetMQSource<dynamic>(
+                pipeline,
+                "audio",
+                "tcp://127.0.0.1:5550",
+                MessagePackFormat.Instance);
 
             var rgbSource = new NetMQSource<dynamic>(
                 pipeline,
@@ -34,6 +41,28 @@ namespace AriaCaptureServer
                 "slam2",
                 "tcp://127.0.0.1:5562",
                 MessagePackFormat.Instance);
+
+            var eyesSource = new NetMQSource<dynamic>(
+                pipeline,
+                "eyes",
+                "tcp://127.0.0.1:5563",
+                MessagePackFormat.Instance);
+
+            var imuSource = new NetMQSource<dynamic>(
+                pipeline,
+                "imu",
+                "tcp://127.0.0.1:5564",
+                MessagePackFormat.Instance,
+                useSourceOriginatingTimes: false); //TODO: fix this
+
+            // Process audio buffers
+            var audioFormat = WaveFormat.Create16BitPcm(48000, 2);
+
+            audioSource.Select(iframe =>
+            {
+                byte[] audioBytes = (byte[])iframe.audio;              
+                return new AudioBuffer(audioBytes, audioFormat);
+            }).Write("Audio", store);
 
             // Start Image Processing 
             rgbSource.Select(iframe =>
@@ -60,7 +89,7 @@ namespace AriaCaptureServer
                 psiImage.Resource.CopyFrom(imageBytes, 0, width * height * channels);
 
                 return psiImage;
-            }).EncodeJpeg().Write("SLAM1", store);
+            }).EncodeJpeg().Write("Slam1", store);
 
             slam2Source.Select(iframe =>
             {
@@ -73,13 +102,28 @@ namespace AriaCaptureServer
                 psiImage.Resource.CopyFrom(imageBytes, 0, width * height * channels);
 
                 return psiImage;
-            }).EncodeJpeg().Write("SLAM2", store);
+            }).EncodeJpeg().Write("Slam2", store);
+
+            eyesSource.Select(iframe =>
+            {
+                int width = (int)iframe.width;
+                int height = (int)iframe.height;
+                int channels = (int)iframe.channels;
+                byte[] imageBytes = (byte[])iframe.image_bytes;
+
+                var psiImage = ImagePool.GetOrCreate(width, height, PixelFormat.Gray_8bpp);
+                psiImage.Resource.CopyFrom(imageBytes, 0, width * height * channels);
+
+                return psiImage;
+            }).EncodeJpeg().Write("Eyes", store);
+
+            imuSource.Select(iframe => ((int)iframe.idx, (double)iframe.accel0, (double)iframe.accel1, (double)iframe.accel2, (double)iframe.gyro0, (double)iframe.gyro1, (double)iframe.gyro2))
+                .Write("IMU", store);
 
             // Run pipeline asynchronously
             pipeline.RunAsync();
-
             Console.WriteLine("Capturing ARIA streams. Press any key to stop recording...");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
--- Sources/Integrations/Aria/AriaCaptureServer/Python/aria_streaming_subscribe_send.py
+++ Sources/Integrations/Aria/AriaCaptureServer/Python/aria_streaming_subscribe_send.py
@@ -18,9 +18,12 @@ import aria.sdk as aria
 import cv2
 import numpy as np
 from common import quit_keypress, update_iptables
+from typing import Sequence
 from psi_com import *
 
 from projectaria_tools.core.sensor_data import ImageDataRecord
+
+import debugpy
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -33,9 +36,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--psi_streams",
         nargs="+",
-        choices=["audio", "rgb", "slam"],
+        choices=["audio", "rgb", "slam", "eyes", "imu", "magneto", "baro"],
         required=True,
-        help="Specify the streams to send to psi: audio, rgb, and/or slam.",
+        help="Specify the streams to send to psi: audio, rgb, slam, eyes, imu, magneto, and/or baro.",
     )
 
     return parser.parse_args()
@@ -44,27 +47,46 @@ def parse_args() -> argparse.Namespace:
 def main():
     args = parse_args()
 
-     # Print the selected PSI stream type
+     # Print the selected PSI stream types
     print(f"Selected psi streams: {args.psi_streams}")
+
+    if "audio" in args.psi_streams:
+        print("Sending audio stream to psi...")
+        audio_socket = setup_output_socket("tcp://127.0.0.1:5550")
+    if "rgb" in args.psi_streams:
+        print("Sending rgb video stream to psi...")
+        rgb_socket = setup_output_socket("tcp://127.0.0.1:5560")
+    if "slam" in args.psi_streams:
+        print("Sending slam video streams to psi...")
+        slam1_socket = setup_output_socket("tcp://127.0.0.1:5561")
+        slam2_socket = setup_output_socket("tcp://127.0.0.1:5562")
+    if "eyes" in args.psi_streams:
+        print("Sending eye video streams to psi...")
+        eyes_socket = setup_output_socket("tcp://127.0.0.1:5563")
+    if "imu" in args.psi_streams:
+        print("Sending IMU streams to psi...")
+        imu_socket = setup_output_socket("tcp://127.0.0.1:5564")
 
     if args.update_iptables and sys.platform.startswith("linux"):
         update_iptables()
 
     #  Optional: Set SDK's log level to Trace or Debug for more verbose logs. Defaults to Info
-    aria.set_log_level(aria.Level.Info)
+    # aria.set_log_level(aria.Level.Info)
 
     # 1. Create StreamingClient instance
     streaming_client = aria.StreamingClient()
 
     #  2. Configure subscription to listen to Aria's RGB and SLAM streams.
     # @see StreamingDataType for the other data types
+    # TODO: This should be set according to the args that were passed in at the top
     config = streaming_client.subscription_config
     config.subscriber_data_type = (
-        aria.StreamingDataType.Rgb | aria.StreamingDataType.Slam
+        aria.StreamingDataType.Rgb | aria.StreamingDataType.Slam | aria.StreamingDataType.Audio | aria.StreamingDataType.EyeTrack | aria.StreamingDataType.Imu
     )
 
     # A shorter queue size may be useful if the processing callback is always slow and you wish to process more recent data
     # For visualizing the images, we only need the most recent frame so set the queue size to 1
+    # TODO: Should we change this setting?
     config.message_queue_size[aria.StreamingDataType.Rgb] = 1
     config.message_queue_size[aria.StreamingDataType.Slam] = 1
 
@@ -84,6 +106,63 @@ def main():
         def on_image_received(self, image: np.array, record: ImageDataRecord):
             self.images[record.camera_id] = image
 
+        def on_imu_received(self, samples: Sequence, imu_idx: int):
+            # TODO: Send all values, from all samples, with accurate timestamps per sample
+            sample = samples[0]
+            imu_message = {
+                "accel0": sample.accel_msec2[0],
+                "accel1": sample.accel_msec2[1],
+                "accel2": sample.accel_msec2[2],
+                "gyro0": sample.gyro_radsec[0],
+                "gyro1": sample.gyro_radsec[1],
+                "gyro2": sample.gyro_radsec[2],
+                "idx": imu_idx}
+            # debugpy.breakpoint()
+            send_topic_message(imu_socket, "imu", imu_message)
+        
+        def on_audio_received(self, audio_and_record, *args):
+            if not hasattr(audio_and_record, "data") or audio_and_record.data is None:
+                print("Received empty audio data")
+                return
+            
+            audio_data = np.array(audio_and_record.data, dtype=np.float32)
+            if audio_data.size == 0:
+                print("Received empty audio data")
+                return        
+
+            # debugpy.breakpoint()
+            # KiranM: Every Step here is very important and the order of execution.
+            # Reshape into 7 channels (assuming the input data is correctly formatted)
+            if audio_data.size % 7 == 0:
+                audio_data = audio_data.reshape(-1, 7).T  # Transpose to shape (7, N)
+            else:
+                raise ValueError(f"Unexpected audio data size: {audio_data.size}, cannot reshape to (7, N)")
+
+            if audio_data.shape[0] != 7:
+                raise ValueError(f"Expected 7-channel audio input, but got shape {audio_data.shape}")
+
+            # KiranM : Apply a balanced stereo mix:
+            # Left: Channels 0, 2, 4 (front-left, left, rear-left) + 50% center
+            # Right: Channels 1, 3, 5 (front-right, right, rear-right) + 50% center
+            left_mix = (audio_data[0] + audio_data[2] + audio_data[4] + 0.5 * audio_data[6]) / 3
+            right_mix = (audio_data[1] + audio_data[3] + audio_data[5] + 0.5 * audio_data[6]) / 3
+
+            # Normalize stereo mix (avoid extreme loudness)
+            max_val = max(np.max(np.abs(left_mix)), np.max(np.abs(right_mix)), 1e-8)
+            left_mix /= max_val
+            right_mix /= max_val
+
+            # Convert to 16-bit PCM format
+            stereo_audio = np.vstack((left_mix, right_mix)).T  # Shape (N, 2)
+            stereo_audio_int16 = np.int16(stereo_audio * 32767)
+            interleaved_audio = stereo_audio_int16.flatten()
+            
+            audio_message = {
+                "audio": interleaved_audio.tobytes(), # audio data
+            }
+
+            send_topic_message(audio_socket, "audio", audio_message)
+
     observer = StreamingClientObserver()
     streaming_client.set_streaming_client_observer(observer)
 
@@ -91,46 +170,30 @@ def main():
     print("Start listening to image data")
     streaming_client.subscribe()
 
-    # KiranM : NetMQ-Protocol
-    # 4. Open A NetMQ Socket to Push the data to the Windows side
-    print("Start hooking up the NetMQ Interface ")
+    # # 5. Visualize the streaming data until we close the window
+    # rgb_window = "Aria RGB"
+    # slam1_window = "Aria SLAM 1"
+    # slam2_window = "Aria SLAM 2"
+    # if "rgb" in args.psi_streams:
+    #     cv2.namedWindow(rgb_window, cv2.WINDOW_NORMAL)
+    #     cv2.resizeWindow(rgb_window, 1024, 1024)
+    #     cv2.setWindowProperty(rgb_window, cv2.WND_PROP_TOPMOST, 1)
+    #     cv2.moveWindow(rgb_window, 50, 50)
 
-    if "audio" in args.psi_streams:
-        print("Sending audio stream to psi...")
-    if "rgb" in args.psi_streams:
-        print("Sending rgb video stream to psi...")
-        rgb_socket = setup_output_socket("tcp://127.0.0.1:5560")
-    if "slam" in args.psi_streams:
-        print("Sending slam video streams to psi...")
-        slam1_socket = setup_output_socket("tcp://127.0.0.1:5561")
-        slam2_socket = setup_output_socket("tcp://127.0.0.1:5562")
-    
-    print("Python sender to Windows pipe is running...")
+    # if "slam" in args.psi_streams:
+    #     cv2.namedWindow(slam1_window, cv2.WINDOW_NORMAL)
+    #     cv2.resizeWindow(slam1_window, 480 , 640)
+    #     cv2.setWindowProperty(slam1_window, cv2.WND_PROP_TOPMOST, 1)
+    #     cv2.moveWindow(slam1_window, 1100, 50)
 
-    # 5. Visualize the streaming data until we close the window
-    rgb_window = "Aria RGB"
-    slam1_window = "Aria SLAM 1"
-    slam2_window = "Aria SLAM 2"
-    if "rgb" in args.psi_streams:
-        cv2.namedWindow(rgb_window, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(rgb_window, 1024, 1024)
-        cv2.setWindowProperty(rgb_window, cv2.WND_PROP_TOPMOST, 1)
-        cv2.moveWindow(rgb_window, 50, 50)
-
-    if "slam" in args.psi_streams:
-        cv2.namedWindow(slam1_window, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(slam1_window, 480 , 640)
-        cv2.setWindowProperty(slam1_window, cv2.WND_PROP_TOPMOST, 1)
-        cv2.moveWindow(slam1_window, 1100, 50)
-
-        cv2.namedWindow(slam2_window, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(slam2_window, 480 , 640)
-        cv2.setWindowProperty(slam2_window, cv2.WND_PROP_TOPMOST, 1)
-        cv2.moveWindow(slam2_window, 1100, 50)
+    #     cv2.namedWindow(slam2_window, cv2.WINDOW_NORMAL)
+    #     cv2.resizeWindow(slam2_window, 480 , 640)
+    #     cv2.setWindowProperty(slam2_window, cv2.WND_PROP_TOPMOST, 1)
+    #     cv2.moveWindow(slam2_window, 1100, 50)
 
     while not quit_keypress():
         # Render the RGB image
-        if aria.CameraId.Rgb in observer.images:
+        if aria.CameraId.Rgb in observer.images and "rgb" in args.psi_streams:
             rgb_image = np.rot90(observer.images[aria.CameraId.Rgb], -1)
             rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB)
 
@@ -150,16 +213,28 @@ def main():
             }            
 
             # Send the serialized data using msgpack
-            if "rgb" in args.psi_streams:
-                send_topic_message(rgb_socket, "rgb", rgb_message, encodeBinary=True)
-                cv2.imshow(rgb_window, rgb_image)
+            send_topic_message(rgb_socket, "rgb", rgb_message, encodeBinary=True)
+            # cv2.imshow(rgb_window, rgb_image)
 
             del observer.images[aria.CameraId.Rgb]
 
+        if aria.CameraId.EyeTrack in observer.images and "eyes" in args.psi_streams:
+            eyes_image = observer.images[aria.CameraId.EyeTrack]
+
+            eyes_message = {            
+                "width": eyes_image.shape[1],   # Width
+                "height": eyes_image.shape[0],  # Height
+                "channels": 1,             # Grayscale (1 channel)
+                "StreamType": 3,          # Stream type identifier
+                "image_bytes": eyes_image.tobytes(), # Actual image data
+            }
+
+            send_topic_message(eyes_socket, "eyes", eyes_message, encodeBinary=True)
+
         # Stack and display the SLAM images
-        if (
-            aria.CameraId.Slam1 in observer.images
+        if (aria.CameraId.Slam1 in observer.images
             and aria.CameraId.Slam2 in observer.images
+            and "slam" in args.psi_streams
         ):
             slam1_image = np.rot90(observer.images[aria.CameraId.Slam1], -1)
             slam2_image = np.rot90(observer.images[aria.CameraId.Slam2], -1)
@@ -194,11 +269,10 @@ def main():
             }            
                                     
             # Send the serialized data using msgpack
-            if "slam" in args.psi_streams:
-               send_topic_message(slam1_socket, "slam1", slam1_message, encodeBinary=True)
-               send_topic_message(slam2_socket, "slam2", slam2_message, encodeBinary=True)
-               cv2.imshow(slam1_window, slam1_buffer)
-               cv2.imshow(slam2_window, slam2_buffer)
+            send_topic_message(slam1_socket, "slam1", slam1_message, encodeBinary=True)
+            send_topic_message(slam2_socket, "slam2", slam2_message, encodeBinary=True)
+            # cv2.imshow(slam1_window, slam1_buffer)
+            # cv2.imshow(slam2_window, slam2_buffer)
             
             del observer.images[aria.CameraId.Slam1]
             del observer.images[aria.CameraId.Slam2]
