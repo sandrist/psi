21cb5342ebf0360bd7c6509da22166d23622861a
Kiran Muthabatulla <kiranm@ntdev.microsoft.com>
2025-04-17 16:20:09 -0700

    cleaned up the code to debug the image path..

--- Sources/Integrations/Aria/AriaCaptureServer/Program.cs
+++ Sources/Integrations/Aria/AriaCaptureServer/Program.cs
@@ -1,22 +1,18 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-
+//
 namespace AriaCaptureServer
 {
     using System;
-    using System.Linq;
+    using System.Linq;    
     using Microsoft.Psi;
     using Microsoft.Psi.Audio;
     using Microsoft.Psi.Imaging;
     using Microsoft.Psi.Interop.Format;
-    using Microsoft.Psi.Interop.Transport;
-    using MessagePack;
+    using Microsoft.Psi.Interop.Transport;    
     using System.Collections.Generic;
-    using System.Dynamic;
-    using OpenCvSharp;
-    using System.Runtime.InteropServices;
-    using System.Collections.Concurrent;
-    using System.Threading;
+    using System.Dynamic;    
 
     internal class Program
     {
@@ -50,62 +46,25 @@ namespace AriaCaptureServer
                 MessagePackFormat.Instance);
 
             var audioSource = new NetMQSource<dynamic>(
-                pipeline,
-                "audio",
-                "tcp://127.0.0.1:5560",
-                MessagePackFormat.Instance);
+               pipeline,
+               "audio",
+               "tcp://127.0.0.1:5560",
+               MessagePackFormat.Instance);
 
-            // === Set up concurrent queue and display thread for RGB ===
-            var displayQueue = new ConcurrentQueue<byte[]>();
-
-            var displayThread = new Thread(() =>
-            {
-                while (true)
-                {
-                    if (displayQueue.TryDequeue(out byte[] imageBytes))
-                    {
-                        try
-                        {
-                            using var mat = new Mat(1408, 1408, MatType.CV_8UC3);
-                            Marshal.Copy(imageBytes, 0, mat.Data, imageBytes.Length);
-                            Cv2.ImShow("RGB Stream", mat);
-                            Cv2.WaitKey(1);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"OpenCV display error: {ex.Message}");
-                        }
-                    }
-                    else
-                    {
-                        Thread.Sleep(1);
-                    }
-                }
-            });
-            displayThread.IsBackground = true;
-            displayThread.Start();
-
-            // === RGB Stream Processing ===
+            // Start Image Processing 
             rgbSource.Select(iframe =>
-            {
+            {            
                 int width = (int)iframe.width;
                 int height = (int)iframe.height;
                 int channels = (int)iframe.channels;
                 byte[] imageBytes = (byte[])iframe.image_bytes;
 
-                // Only queue frame for display if there's space
-                if (displayQueue.Count < 2)
-                {
-                    displayQueue.Enqueue(imageBytes);
-                }
-
                 var psiImage = ImagePool.GetOrCreate(height, width, PixelFormat.BGR_24bpp);
                 psiImage.Resource.CopyFrom(imageBytes, 0, width * height * channels);
-
+                                
                 return psiImage;
             }).EncodeJpeg().Write("RGB", store);
 
-            // === SLAM 1 Stream ===
             slam1Source.Select(iframe =>
             {
                 int width = (int)iframe.width;
@@ -119,7 +78,6 @@ namespace AriaCaptureServer
                 return psiImage;
             }).EncodeJpeg().Write("Slam1", store);
 
-            // === SLAM 2 Stream ===
             slam2Source.Select(iframe =>
             {
                 int width = (int)iframe.width;
@@ -127,13 +85,12 @@ namespace AriaCaptureServer
                 int channels = (int)iframe.channels;
                 byte[] imageBytes = (byte[])iframe.image_bytes;
 
-                var psiImage = ImagePool.GetOrCreate(height, width, PixelFormat.Gray_8bpp);
+                var psiImage = ImagePool.GetOrCreate(height,width, PixelFormat.Gray_8bpp);
                 psiImage.Resource.CopyFrom(imageBytes, 0, width * height * channels);
 
                 return psiImage;
             }).EncodeJpeg().Write("Slam2", store);
 
-            // === Eyes Stream ===
             eyesSource.Select(iframe =>
             {
                 int width = (int)iframe.width;
@@ -141,13 +98,12 @@ namespace AriaCaptureServer
                 int channels = (int)iframe.channels;
                 byte[] imageBytes = (byte[])iframe.image_bytes;
 
-                var psiImage = ImagePool.GetOrCreate(height, width, PixelFormat.Gray_8bpp);
+                var psiImage = ImagePool.GetOrCreate(width, height, PixelFormat.Gray_8bpp);
                 psiImage.Resource.CopyFrom(imageBytes, 0, width * height * channels);
 
                 return psiImage;
             }).EncodeJpeg().Write("Eyes", store);
 
-            // === Audio Stream ===
             var audioFormat = WaveFormat.Create16BitPcm(48000, 2);
 
             audioSource.Select(iframe =>
@@ -159,7 +115,7 @@ namespace AriaCaptureServer
 
             }).Write("Audio", store);
 
-            // === Start pipeline ===
+            // Run pipeline asynchronously
             pipeline.RunAsync();
             Console.WriteLine("Capturing ARIA streams. Press any key to stop recording...");
             Console.ReadKey();
