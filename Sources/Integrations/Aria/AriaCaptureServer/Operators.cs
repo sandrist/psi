7a14892bd9a0dd1d1e2bd5b5ca19ef69d8224b50
Kiran Muthabatulla <kiranm@ntdev.microsoft.com>
2025-07-08 15:52:31 -0700

    hands is working.. operators are good.. hands opertor is tested.. not the others.. they may not work..

--- Sources/Integrations/Aria/AriaCaptureServer/Operators.cs
+++ Sources/Integrations/Aria/AriaCaptureServer/Operators.cs
@@ -50,5 +50,144 @@ namespace AriaCaptureServer
                 var byteData = (byte[])messageDict["values"];
                 return new AudioBuffer(byteData, audioFormat);
             }, deliveryPolicy);
+
+
+
+
+        public static IProducer<List<Vector3>> ProcessHands(this IProducer<dynamic> inputStream, DeliveryPolicy deliveryPolicy = null) =>
+    inputStream.Process<dynamic, List<Vector3>>((iframe, _, emitter) =>
+    {
+        try
+        {
+            // Extract timestamp safely from iframe
+            DateTime timestamp;
+            try
+            {
+                timestamp = new DateTime((long)iframe.originatingTime);
+            }
+            catch
+            {
+                timestamp = DateTime.UtcNow;
+            }
+
+            // Iterate over each hand
+            foreach (var handObj in iframe.values)
+            {
+                var handPoints = new List<Vector3>();
+
+                try
+                {
+                    var pointList = (IEnumerable<object>)handObj;
+
+                    foreach (var pt in pointList)
+                    {
+                        var coords = (IList<object>)pt;
+                        float x = Convert.ToSingle(coords[0]);
+                        float y = Convert.ToSingle(coords[1]);
+                        float z = Convert.ToSingle(coords[2]);
+
+                        handPoints.Add(new Vector3(x, y, z));
+                    }
+
+                    emitter.Post(handPoints, timestamp);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[ProcessHands] Hand parse error: {ex.Message}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[ProcessHands] Frame error: {ex.Message}");
+        }
+    }, deliveryPolicy);
+
+
+
+
+        public static IProducer<List<Vector3>> ProcessSkeleton(this IProducer<dynamic> inputStream, DeliveryPolicy deliveryPolicy = null) =>
+            inputStream.Process<dynamic, List<Vector3>>((iframe, _, emitter) =>
+            {
+                try
+                {
+                    var joints = new List<Vector3>();
+
+                    foreach (var joint in iframe.values)
+                    {
+                        var point = (IList<object>)joint;
+                        float x = Convert.ToSingle(point[0]);
+                        float y = Convert.ToSingle(point[1]);
+                        float z = Convert.ToSingle(point[2]);
+                        joints.Add(new Vector3(x, y, z));
+                    }
+
+                    // Try to get timestamp from iframe or fallback
+                    DateTime timestamp;
+                    try
+                    {
+                        timestamp = iframe.originatingTime != null ? new DateTime((long)iframe.originatingTime) : DateTime.UtcNow;
+                    }
+                    catch
+                    {
+                        timestamp = DateTime.UtcNow;
+                    }
+
+                    emitter.Post(joints, timestamp);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[ProcessSkeleton] Error: {ex.Message}");
+                }
+            }, deliveryPolicy);
+
+
+        public static IProducer<List<(Vector2 EyeCenter, Vector2 IrisCenter, Vector2 GazeVector)>> ProcessGaze(this IProducer<dynamic> inputStream, DeliveryPolicy deliveryPolicy = null) =>
+            inputStream.Process<dynamic, List<(Vector2, Vector2, Vector2)>>((iframe, _, emitter) =>
+            {
+                try
+                {
+                    var gazeList = new List<(Vector2 EyeCenter, Vector2 IrisCenter, Vector2 GazeVector)>();
+
+                    foreach (var eye in iframe.values) // eye = [eyeCenter, irisCenter, gazeVector]
+                    {
+                        var eyeList = (IList<object>)eye;
+
+                        var eyeCenterList = (IList<object>)eyeList[0];
+                        var irisCenterList = (IList<object>)eyeList[1];
+                        var gazeVectorList = (IList<object>)eyeList[2];
+
+                        var eyeCenter = new Vector2(Convert.ToSingle(eyeCenterList[0]), Convert.ToSingle(eyeCenterList[1]));
+                        var irisCenter = new Vector2(Convert.ToSingle(irisCenterList[0]), Convert.ToSingle(irisCenterList[1]));
+                        var gazeVector = new Vector2(Convert.ToSingle(gazeVectorList[0]), Convert.ToSingle(gazeVectorList[1]));
+
+                        gazeList.Add((eyeCenter, irisCenter, gazeVector));
+                    }
+
+                    // Try to get timestamp from iframe or fallback
+                    DateTime timestamp;
+                    try
+                    {
+                        timestamp = iframe.originatingTime != null ? new DateTime((long)iframe.originatingTime) : DateTime.UtcNow;
+                    }
+                    catch
+                    {
+                        timestamp = DateTime.UtcNow;
+                    }
+
+                    emitter.Post(gazeList, timestamp);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[ProcessGaze] Error: {ex.Message}");
+                }
+            }, deliveryPolicy);
+
+
+
+
+
+
+
     }
 }
--- Sources/Integrations/Aria/AriaCaptureServer/Program.cs
+++ Sources/Integrations/Aria/AriaCaptureServer/Program.cs
@@ -3,13 +3,14 @@
 
 namespace AriaCaptureServer
 {
-    using System;
-    using System.Numerics;
     using Microsoft.Psi;
     using Microsoft.Psi.Audio;
     using Microsoft.Psi.Imaging;
     using Microsoft.Psi.Interop.Format;
     using Microsoft.Psi.Interop.Transport;
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
 
     internal class Program
     {
@@ -48,6 +49,12 @@ namespace AriaCaptureServer
             var magnetoSource = CreateSource("magneto", 5558);
             var baroSource = CreateSource("baro", 5559);
 
+
+            var handsSource = CreateSource("hands", 5561);
+            var skeletonSource = CreateSource("skeleton", 5562);
+            var gazeSource = CreateSource("gaze", 5563);
+
+
             // Start Image Processing 
             rgbSource.ProcessImage(PixelFormat.BGR_24bpp).EncodeJpeg().Write("RGB", store);
             slam1Source.ProcessImage(PixelFormat.Gray_8bpp).EncodeJpeg().Write("Slam1", store);
@@ -58,6 +65,11 @@ namespace AriaCaptureServer
             var audio = audioSource.ProcessAudio(WaveFormat.CreatePcm(48000, 32, 7), DeliveryPolicy.Unlimited);
             audio.Write("Audio", store, deliveryPolicy: DeliveryPolicy.Unlimited);
             audio.Resample(WaveFormat.Create16kHz1Channel16BitPcm(), DeliveryPolicy.Unlimited).Write("ResampledAudio", store, deliveryPolicy: DeliveryPolicy.Unlimited);
+
+            handsSource.ProcessHands().Write("Hands", store);
+            
+            // skeletonSource.ProcessSkeleton().Write("Skeleton", store);
+            // gazeSource.ProcessGaze().Write("Gaze", store);
 
             // Process IMU and other data
             accel0Source.ProcessIMU().Write("Accel0", store);
